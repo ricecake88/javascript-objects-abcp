34948e1f4dabd171e4566620d6cfbc939242442c
Grace Shih <vocalise@gmail.com>
2018-01-13 19:20:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,7 @@ var playlist = {'Sarah Mclachlan':'I Will Remember You',
 
 function updatePlaylist(myPlaylist, artistName, songName) {
  Object.assign(myPlaylist, {[artistName]: songName});
+ console.log(myPlaylist);
  return myPlaylist;
 }
 
