9fd74b86740c9e84068456260a1784c4ccb69e59
Grace Shih <vocalise@gmail.com>
2018-01-13 18:42:03 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {'Sarah Mclachlan':'I Will Remember You',
+'Madonna':'Vogue',
+'Michael Jackson':'Thriller'
+}
+
+function updatePlaylist(myPlaylist, artistName, songName) {
+  var newPlaylist = 
+  return
+}
